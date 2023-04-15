f9f4bf4a94533e7ca1f382a516c14a43f2fbfc3b
Vashu Agarwal <vashuag9@gmail.com>
2023-04-15 23:47:35 +0530

    added animation on intropage

--- app/src/main/java/com/vashu/swiftflow/SplashScreen.kt
+++ app/src/main/java/com/vashu/swiftflow/SplashScreen.kt
@@ -1,23 +1,45 @@
 package com.vashu.swiftflow
 
+import android.content.Intent
 import android.graphics.Typeface
-import androidx.appcompat.app.AppCompatActivity
+import android.os.Build
 import android.os.Bundle
+import android.os.Handler
+import android.view.WindowInsets
 import android.view.WindowManager.LayoutParams.FLAG_FULLSCREEN
+import androidx.appcompat.app.AppCompatActivity
+
 
 class SplashScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
-        window.setFlags(
-            FLAG_FULLSCREEN,
-            FLAG_FULLSCREEN
 
-        )
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            // Use the new API for Android 11 and higher
+            val insetsController = window.insetsController
+            insetsController?.hide(WindowInsets.Type.statusBars() or WindowInsets.Type.navigationBars())
+        } else {
+            // Use the old deprecated way for pre-Android 11
+            window.setFlags(
+                FLAG_FULLSCREEN,
+                FLAG_FULLSCREEN
+            )
+        }
+
 
 //        val typeFace: Typeface = Typeface.createFromAsset((assets,""))
 //
 //        tv_app_name.typeface =typeFace
+
+        Handler().postDelayed({
+                              startActivity(Intent(this,IntroActivity::class.java))
+            finish()
+
+
+        },2500)
+
+
 
     }
 }
